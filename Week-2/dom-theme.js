7c7bdf9b0a8590763a87944af5bb81320db58b90
Rayce303 <orullianrayce@gmail.com>
2026-09-23 15:02:21 -0600

    Dom Theme
    I did those

---
+++ Week-2/dom-theme.js
@@ -0,0 +1,42 @@
+// Functions
+function thisFunction(word) {
+    console.log(word.target.value);
+}
+
+// call dat function oooooh boy
+// thisFunction("waffles");
+
+// Event Listeners
+let select = document.querySelector("#theme-select");
+
+// event listeners require 1. a thing that happens and
+// 2. a function to execute as a result
+// do NOT put prenthesis in these function names
+select.addEventListener("change", thisFunction);
+
+// IF statements
+
+
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
+          
